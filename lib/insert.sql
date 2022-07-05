6176c6903b999d6005b4339690a34762c8004ae9
--replace-all <terrencejung928@gmail.com>
2022-07-05 10:54:25 -0700

    commit tree 957a8c56834d76d5a5b9c3452908d21dd56b3cd3

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 8, "M", "Brown", "Calm", 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Rowdy", 12, "M", "White", "Violent", 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Tabitha", 7, "W", "Black", "Calm", 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 14, "M", "Brown", "Violent", 0);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Melissa", 14, "W", "Black", "Calm", 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Grinch", 15, "M", "Green", "Violent", 0);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Wendy", 2, "W", "Brown", "Playful", 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES (NULL, 100, "M", "Rainbow", "Mysterious", 1);
